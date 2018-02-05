1e3f755b7f52342fd2e70f32b2fc23cd77acde12
fibrahimb <fibrahimb@berkeley.edu>
2018-02-05 08:50:35 -0800

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ if(isset($_POST['submit'])){
     $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    header('Location: contact-thank-you.html');
+    header('Location: index/contact-thank-you.html');
     exit();
     }
 ?>
